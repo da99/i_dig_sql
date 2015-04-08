87a7ba833b11e15803b48e72f0fb9c207bd0eb06
da99 <i-hate-spam-1234567@mailinator.com>
2015-04-08 15:12:01 -0500

    Dev. checkpoint.

--- lib/i_dig_sql.rb
+++ lib/i_dig_sql.rb
@@ -41,9 +41,10 @@ class I_Dig_Sql
       a.is_a? I_Dig_Sql
     }
 
-    @current_def = nil
-    @sqls        = H.new
-    @vars        = H.new
+    @sqls = H.new
+    @vars = H.new
+
+    @current_def  = nil
 
     @sqls.merge_these *(@digs.map(&:sqls))
     @vars.merge_these *(@digs.map(&:vars))
@@ -78,82 +79,49 @@ class I_Dig_Sql
     [to_sql, vars]
   end
 
-  def to_sql
-    @FINAL = {
-      WITH: [],
-      FROM: [],
-      FROM_AS: H.new,
-      WHERE: [],
-      WHERE_AS: H.new,
-      GROUP_BY: [],
-      HAVING: [],
-      LIMIT: [],
-      OFFSET: [],
-      RAW: []
+  def to_sql target_name = nil
+    final = {
+      WITH:     [],
+      RAW:      []
     }
 
-    fragments = @fragments.dup
+    fail ArgumentError, "No query defined." if !target_name && @fragments.empty?
 
-    while s = fragments.shift
-      next unless s.is_a?(String)
+    fragments_to_raw(@fragments, final)
+    table_name_to_raw(target_name, final) if target_name
+    with_to_raw(final)
 
-      @FINAL[:RAW] << s
-      while s[HAS_VAR] 
-        s.gsub!(/\{\{\s?([a-zA-Z0-9\_]+)\s?\}\}/) do |match|
-          key = $1.to_sym
-          @FINAL[:WITH] << key
-          key
-        end
+    string = ""
 
-        s.gsub!(/\<\<\s?([a-zA-Z0-9\_\-\ \*]+)\s?\>\>/) do |match|
-          tokens = $1.split
-          key    = tokens.pop.to_sym
-          field  = tokens.empty? ? nil : tokens.join(' ')
+    if !final[:WITH].empty?
+      string << (%^WITH\n  #{final[:WITH].join ",\n  "}\n^)
+    end
 
-          case
-          when field
-            @FINAL[:WITH] << key
-            "SELECT #{field} FROM #{key}"
-          else
-            self[key]
-          end
-        end
-      end # === while s HAS_VAR
+    string << (final[:RAW].join "\n")
 
-    end # === while
-
-    [
-      :WITH, :FROM, :WHERE, :GROUP_BY,
-      :HAVING, :LIMIT, :OFFSET, :RAW
-    ].map { |name|
-      next if @FINAL[name].empty?
-      case name
-      when :WITH
-        %^
-          WITH
-          #{@FINAL[name].uniq.map { |k| "#{k} AS (
-            #{self[k]}
-          )" }.join "
-          ,
-          "}
-        ^
-      when :FROM
-      when :WHERE
-      when :GROUP_BY
-      when :HAVING
-      when :LIMIT
-      when :OFFSET
-      when :RAW
-        @FINAL[name]
-      end
-    }.flatten.compact.join "\n".freeze
+    string
   end # === def to_sql
 
   # === LINK DSL ====================================================
   def def name
-    @sqls[name] = {:name=>name}
+    @sqls[name] = H.new.merge!(
+      :name       => name,
+      :order_by   => [],
+      :group_by   => [],
+      :select     => [],
+      :from       => [],
+      :type_ids   => []
+      # :out        => nil,
+      # :in         => nil,
+      # :out_ftable => nil,
+      # :in_ftable  => nil,
+      # :out_has    => nil,
+      # :in_has     => nil,
+      # :in_not_in  => nil,
+      # :out_not_in => nil
+    )
     old = @current_def
-    @current_def= @sqls[name]
+    @current_def = @sqls[name]
     instance_eval &Proc.new
     @current_def = old
     self
@@ -191,36 +159,31 @@ class I_Dig_Sql
   end
 
   def order_by *args
-    @current_def[:order_by] ||= []
     @current_def[:order_by] << args
     self
   end
 
   def group_by *args
-    @current_def[:group_by] ||= []
     @current_def[:group_by] << args
     self
   end
 
   def select *args
-    @current_def[:select] ||= []
     @current_def[:select] << args
     self
   end
 
   def get *args
-    @current_def[:from] ||= []
-    @current_def[:from] << args
+    @current_def[:from].concat args
     self
   end
 
   def of name
-    @current_def[:start] = name
+    @current_def[:of] = name
   end
 
   def type_id name
     old = @current_def
-    old[:type_ids] ||= []
     old[:type_ids] << {}
     @current_def = old[:type_ids].last
     instance_eval(&Proc.new) if block_given?
@@ -228,6 +191,165 @@ class I_Dig_Sql
     self
   end
   # =================================================================
+
+  # === Rendering DSL ===============================================
+
+  def fragments_to_raw str_or_arr, final = {:WITH=>[], :RAW=>[]}
+    raws = if str_or_arr.is_a?(String)
+             [str_or_arr]
+           else
+             str_or_arr
+           end
+
+    fragments = raws.dup
+    while s = fragments.shift
+      next unless s.is_a?(String)
+
+      final[:RAW] << s
+      while s[HAS_VAR] 
+        s.gsub!(/\{\{\s?([a-zA-Z0-9\_]+)\s?\}\}/) do |match|
+          key = $1.to_sym
+          final[:WITH] << key
+          key
+        end
+
+        s.gsub!(/\<\<\s?([a-zA-Z0-9\_\-\ \*]+)\s?\>\>/) do |match|
+          tokens = $1.split
+          key    = tokens.pop.to_sym
+          field  = tokens.empty? ? nil : tokens.join(' ')
+
+          case
+          when field
+            final[:WITH] << key
+            "SELECT #{field} FROM #{key}"
+          else
+            self[key]
+          end
+        end
+      end # === while s HAS_VAR
+
+    end # === while
+
+    final
+  end # === fragments_to_raw
+
+  def table_name_to_raw key, final
+    target = self[key]
+    if target.is_a?(String)
+      final[:RAW] << target
+    else
+
+      final[:RAW] << "-- i_dig_sql: #{key.inspect}\n"
+
+      if target.has_key?(:in) && target.has_key?(:out)
+        final[:RAW] <<(
+          target.map { |k,v|
+            "#{k}: #{v.inspect}"
+          }.join ",\n    "
+        )
+      end
+    end
+
+    [
+      :select, :from, :of, :group_by
+    ].each { |name|
+      case name
+
+      when :select
+        final[:SELECT].concat(target[:select] || ['*'])
+
+      when :from
+        final[:WITH].concat target[:from]
+        final[:FROM].concat target[:from]
+
+      when :of
+        next unless target.has_key?(:of)
+        final[:WHERE] << %^#{target[:from].first}.owner_id = #{target[:of].inspect}^
+
+      when :group_by
+        final[:GROUP_BY].concat target[:group_by]
+
+      else
+        fail "Programmer Error: unknown key #{name.inspect}"
+      end # === case name
+    } # === string
+
+
+  end # === def cte_to_string
+
+  def with_to_raw final
+    withs = final[:WITH].dup
+
+    while k = withs.shift
+      meta = case k
+             when Symbol
+               self[k]
+             when String
+               k
+             when Hash
+               k
+             else
+               fail ArgumentError, "Unknown type for :WITH: #{k.class}"
+             end
+    end # === while
+
+  end # === def with_to_raw
+
+  def hash_to_with_and_string h
+    final = {
+      WITH:     [],
+      SELECT:   [],
+      FROM:     [],
+      FROM_AS:  H.new,
+      WHERE:    [],
+      WHERE_AS: H.new,
+      GROUP_BY: [],
+      HAVING:   [],
+      LIMIT:    [],
+      OFFSET:   [],
+      RAW:      []
+    }
+    string = clauses.map { |name|
+
+      next if final[name] && final[name].empty?
+
+      case name
+
+      when :SELECT
+        %^SELECT\n  #{final[name].flatten.join ",\n  "}\n^
+
+      when :FROM
+        joins = []
+        final[name].each_with_index { |v, i|
+          joins << case i
+          when 0
+            v
+          else
+            " INNER JOIN #{v}\n    ON some.id = other.id "
+          end
+        }
+        %^FROM\n  #{joins.join "\n "}\n^
+
+      when :WHERE
+        %^WHERE\n  #{final[name].join ",\n  "}\n^
+
+      when :GROUP_BY
+        %^GROUP BY\n #{final[name].join ",\n  "}^
+
+      when :HAVING
+
+      when :LIMIT
+
+      when :OFFSET
+
+      else
+        fail "Programmer error: Not dealt: #{name.inspect}"
+
+      end # === case
+    }.flatten.compact.join "\n".freeze
+  end # === def hash_to_with_and_string
+
+  # === END: Rendering DSL ==========================================
 
 end # === class I_Dig_Sql ===
 
--- specs/0010-links.rb
+++ specs/0010-links.rb
@@ -5,18 +5,18 @@ describe "links DSL" do
     sql = I_Dig_Sql.new
 
     sql.def(:block) do
-      out_in(:blocked, :victim) do
+      out_in(:blocked, :victim) {
         are  :screen_name
         have :owner_id
-      end
+      }
 
-      type_id(:BLOCK_SCREEN) do
+      type_id(:BLOCK_SCREEN) {
         out_in(:owner_id, :victim)
-      end
+      }
 
-      type_id(:BLOCK_OWNER) do
+      type_id(:BLOCK_OWNER) {
         out_in(:owner_id, :owner_id)
-      end
+      }
     end
 
     sql.def(:post) do
@@ -39,22 +39,25 @@ describe "links DSL" do
 
     sql.def(:feed) do
 
+      get :follow, :post
+      of  :audience_id
+      group_by 'follow.star'
+
       select(
         'follow.star.screen_name',
         'post.*', 
         'max(post.pub.created_at)'
       )
 
-      get :follow, :post
-      of  :audience_id
-
-      group_by 'follow.star'
-
     end # === query
 
+    actual = sql.to_sql(:feed)
 
-    puts sql.to_sql
-    sql.to_sql.should == "a"
+    puts "################################"
+    puts actual
+    puts "################################"
+
+    sql(actual).should == "a"
   end # === it
 
 end # === describe "links DSL"
